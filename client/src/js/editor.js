a7e4696b4a4b1136ffbe3e96eacc083ad5a2dc0c
Nick Menzhuber <nickmenzhuber@gmail.com>
2023-01-23 15:16:33 -0600

    debugged with thanks to AskBCS Slackbot

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,9 +25,10 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.info('Loaded data from IndexedDB, injecting into editor', data);
+      this.editor.setValue(data.content || localData || header);
     });
+    console.log("New console log test")
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
